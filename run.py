75f09ec8e6136ca9f5dc806005cd0e3b43bfa4fd
Craig Dickerson <craigdickerson@hotmail.com>
2024-08-06 08:39:11 +0000

    Create game class, define methods and attributes for game

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from colorama import Fore, Back, Style, init
+import os
+import sys
+
+"""
+To initialize colorama, which is needed to color the quadrants
+"""
+init()
+
+"""
+This class will define the game's methods and attributes
+"""
+class PyQuadrants:
+    def __init__(self):
+        self.reset_game()
+        self.colors = {
+            'quadrant1': Back.RED,
+            'quadrant2': Back.GREEN,
+            'quadrant3': Back.YELLOW,
+            'quadrant4': Back.BLUE,
+            'endc': Style.RESET_ALL
+        }
